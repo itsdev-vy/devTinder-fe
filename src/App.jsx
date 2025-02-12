3aad70350a346704a2bfc4e7376a90fdaae1f7be
vinay <itsvinayadav@gmail.com>
2025-02-12 18:47:49 +0530

    Add Feed component to routing and implement logout functionality in Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router';
 import Home from './routes/home/Home';
 import Login from './view/login/Login';
 import Profile from './view/profile/profile';
+import Feed from './view/feed/Feed';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Home />}>
+            <Route path="/" element={<Feed />} />
             <Route path="/login" element={<Login />} />
             <Route path="/profile" element={<Profile />} />
           </Route>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router';
+import { removeUser } from '../../redux/slice/user/userSlice';
+import axios from 'axios';
+import { BASE_URL } from '../../utils/constants';
 
 const Navbar = () => {
   const User = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(
+        BASE_URL + '/logout',
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(removeUser());
+      navigate('/login');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="navbar bg-base-300 px-5">
       <div className="flex-1">
@@ -38,7 +60,9 @@ const Navbar = () => {
                 <Link to={'/settings'}>Settings</Link>
               </li>
               <li>
-                <Link to={'/logout'}>Logout</Link>
+                <div className="btn btn-ghost" onClick={handleLogout}>
+                  Logout
+                </div>
               </li>
             </ul>
           </div>
