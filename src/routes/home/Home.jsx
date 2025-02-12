b96b62cb9d2fa4f8767fa352bbaf029fe83ac478
vinay <itsvinayadav@gmail.com>
2025-02-12 21:33:59 +0530

    Fetch user data on Home component mount and navigate to login on unauthorized access

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../../components/navbar/Navbar';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigate } from 'react-router';
 import Footer from '../../components/footer/Footer';
+import { useDispatch, useSelector } from 'react-redux';
+import { BASE_URL } from '../../utils/constants';
+import { addUser } from '../../redux/slice/user/userSlice';
+import axios from 'axios';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.user);
+
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const response = await axios.get(BASE_URL + '/profile/view', {
+        withCredentials: true,
+      });
+      dispatch(addUser(response?.data));
+    } catch (error) {
+      if (error?.status === 401) {
+        navigate('/login');
+      }
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
       <div>
